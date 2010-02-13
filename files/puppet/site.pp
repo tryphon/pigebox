0380d9c27ebb090dbb2e38fd0b0443b31635a3f8
Alban Peignier <alban@tryphon.eu>
2010-02-13 11:32:20 +0100

    Create runtime puppet configuration with /etc/puppet/site.pp. Refs #8

---
+++ files/puppet/site.pp
@@ -0,0 +1,27 @@
+# Defaults
+
+Exec { 
+  path => "/usr/bin:/usr/sbin/:/bin:/sbin:/usr/local/bin:/usr/local/sbin" 
+}
+
+File { 
+  checksum => md5, owner => root, group => root
+}
+
+# Directories used by Pige
+
+file { ["/srv/pige/chunks", "/srv/pige/db"]:
+  ensure => directory,
+  owner => www-data
+}
+
+exec { "pige-create-db":
+  creates => "/srv/pige/db/production.sqlite3",
+  command => "cp /usr/share/pige/db/production.sqlite3 /srv/pige/db/production.sqlite3",
+  require => File["/srv/pige/db"]
+}
+
+file { "/srv/pige/db/production.sqlite3":
+  owner => www-data,
+  require => Exec["pige-create-db"]
+}
--- manifests/classes/puppet.pp
+++ manifests/classes/puppet.pp
@@ -4,4 +4,18 @@ class puppet {
     source => "$source_base/files/puppet/puppet.init",
     mode => 755
   }
+
+  file { "/etc/puppet/site.pp":
+    source => "$source_base/files/puppet/site.pp"
+  }
+
+  file { "/usr/local/sbin/launch-puppet":
+    source => "$source_base/files/puppet/launch-puppet",
+    mode => 755
+  }
+
+  file { "/etc/cron.d/puppet":
+    source => "$source_base/files/puppet/puppet.cron.d",
+    require => Package[cron]
+  }
 }
