38b8973a5df428ab99eab8e809d78d5292326540
Alban Peignier <alban@tryphon.eu>
2010-04-12 19:16:13 +0200

    Workaround to execute amixerconf in pige.cron. Refs #27

--- files/pige/pige.cron.d
+++ files/pige/pige.cron.d
@@ -5,3 +5,6 @@
 MAILTO=root
 1,16,31,46 * * * *     pige   /usr/share/pige/bin/pige-cron
 @reboot                www-data   /usr/share/pige/bin/pige index /srv/pige/records
+
+# FIXME
+@reboot                root   /usr/local/bin/amixerconf
