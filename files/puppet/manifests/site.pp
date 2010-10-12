84c50327f7ccd7df646814a4bb48f20699b6acbf
Florent Peyraud <florent@tryphon.eu>
2010-10-12 23:39:29 +0200

    fm.conf must be on top level

--- files/puppet/manifests/site.pp
+++ files/puppet/manifests/site.pp
@@ -72,3 +72,19 @@ exec { "copy-var-model":
   command => "cp -a /var/log.model/* /var/log/",
   tag => boot
 }
+
+file { "/var/etc/fm/":
+  ensure => directory,
+  tag => boot
+}
+
+file { "/var/etc/fm/fm.conf":
+  content => template("/etc/puppet/templates/fm.conf"),
+  notify => Service[fm],
+  tag => boot
+}
+
+service { fm:
+  ensure => running,
+  hasrestart => true
+}
