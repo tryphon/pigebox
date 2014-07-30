09ed2e865ee763e22db2afd8ddb74f450dd6e0c1
Alban Peignier <alban@tryphon.eu>
2014-07-30 10:37:08 +0200

    Update PigeControl to fix chunks/new in fr locale

--- manifests/classes/pige.pp
+++ manifests/classes/pige.pp
@@ -69,7 +69,7 @@ class pige::frontend {
 
   include apt::tryphon::dev
   package { pigecontrol:
-    ensure => '0.19-1+build2223',
+    ensure => '0.19-1+build2224',
     require => [Apt::Source[tryphon-dev], Package[libapache2-mod-passenger], Package[sox]],
   }
 
