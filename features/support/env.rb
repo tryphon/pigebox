b498b612b504d676ea5c271b248728de56a13572
Alban Peignier <alban@tryphon.eu>
2015-03-27 16:09:10 +0100

    Use poltergeist in cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,13 +9,12 @@ require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumb
 
 require 'capybara/cucumber'
 require 'capybara/session'
-require 'capybara/mechanize'
+require 'capybara/poltergeist'
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
-
-Capybara.default_driver = :mechanize
+Capybara.default_driver = :poltergeist
 Capybara.app_host = "http://pigebox.local"
