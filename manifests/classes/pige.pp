2e7ce781db8d433746304b9a2a53499c6ae12148
Alban Peignier <alban@tryphon.eu>
2015-04-28 21:14:15 +0200

    Update pigecontrol (ftp upload authentication and upload file clean)

--- manifests/classes/pige.pp
+++ manifests/classes/pige.pp
@@ -32,7 +32,7 @@ class pige::frontend {
 
   include apt::tryphon::dev
   package { pigecontrol:
-    ensure => '0.19-1+build2240',
+    ensure => '0.19-1+build2241',
     require => [Apt::Source[tryphon-dev], Package[libapache2-mod-passenger], Package[sox]],
   }
 
